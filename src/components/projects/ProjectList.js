2ae8f91ab83f693eccd6af84aeefbe3ef8e37bda
islom247 <islomiddin.sodiqov@gmail.com>
2019-09-21 23:05:21 +0300

    Dashboard and Notifications panel added

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,29 @@
+import React from "react";
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title ">Project Title</span>
+          <p>Posted by NN</p>
+          <p className="grey-text">21st September 22:56</p>
+        </div>
+      </div>
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title ">Project Title</span>
+          <p>Posted by NN</p>
+          <p className="grey-text">21st September 22:56</p>
+        </div>
+      </div>
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title ">Project Title</span>
+          <p>Posted by NN</p>
+          <p className="grey-text">21st September 22:56</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectList;
