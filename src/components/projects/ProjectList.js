1a0b2e87709c56ac2fff16f7c20b45cf0cd120d6
islom247 <islomiddin.sodiqov@gmail.com>
2019-09-21 23:16:30 +0300

    Project Summary added
    instead of writing lots of div for each project's summary a new functional component will do it for us

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,28 +1,12 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
+
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project Title</span>
-          <p>Posted by NN</p>
-          <p className="grey-text">21st September 22:56</p>
-        </div>
-      </div>
-      <div className="card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project Title</span>
-          <p>Posted by NN</p>
-          <p className="grey-text">21st September 22:56</p>
-        </div>
-      </div>
-      <div className="card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project Title</span>
-          <p>Posted by NN</p>
-          <p className="grey-text">21st September 22:56</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">Project Title</span>
+        <p>Posted by NN</p>
+        <p className="grey-text">21st September 22:56</p>
+      </div>
+    </div>
+  );
+};
+export default ProjectSummary;
