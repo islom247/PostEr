5f576bd5e08ce33b51fede44211a9adededa7d9a
islom247 <islomiddin.sodiqov@gmail.com>
2019-09-25 21:39:55 +0300

    redux store created and reducer(s) added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import "./index.css";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
