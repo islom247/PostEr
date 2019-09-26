c56929d77009596d7bdb8eec803f9e45d7c79010
islom247 <islomiddin.sodiqov@gmail.com>
2019-09-26 13:35:04 +0300

    firebase configured

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA0VWeEstk0mTmWwUWHjoEZkj6hjnBgnek",
+  authDomain: "react-firebase-tutorial-f7.firebaseapp.com",
+  databaseURL: "https://react-firebase-tutorial-f7.firebaseio.com",
+  projectId: "react-firebase-tutorial-f7",
+  storageBucket: "",
+  messagingSenderId: "281411784671",
+  appId: "1:281411784671:web:9418212c42b54f41447e6a",
+  measurementId: "G-QJBT89EKQZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timeStampInSnapshots: true });
+export default firebase;
