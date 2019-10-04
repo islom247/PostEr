4d3666816930645251d7d0b998eb5d70cfb31122
islom247 <islomiddin.sodiqov@gmail.com>
2019-10-04 20:50:43 +0300

    fetches profile from db and and saves to firebase reducer
    long story short, initials now show the correct letters for each user

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,25 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-const Navbar = (props) => {
-  const {auth} = props;
-  const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
+const Navbar = props => {
+  const { auth, profile } = props;
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           MarioPlan
         </Link>
-        { links }
+        {links}
       </div>
     </nav>
   );
 };
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+  console.log(state);
   return {
-    auth: state.firebase.auth
-  }
-}
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  };
+};
 export default connect(mapStateToProps)(Navbar);
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {connect} from "react-redux";
-import {signOut} from "../../store/actions/authActions";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-const SignedInLinks = (props) => {
+const SignedInLinks = props => {
   return (
     <ul className="right">
       <li>
@@ -14,15 +14,18 @@ const SignedInLinks = (props) => {
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
-          NN
+          {props.profile.initials}
         </NavLink>
       </li>
     </ul>
   );
 };
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    signOut : () => dispatch(signOut())
-  }
-}
-export default connect(null, mapDispatchToProps)(SignedInLinks);
+    signOut: () => dispatch(signOut())
+  };
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedInLinks);
