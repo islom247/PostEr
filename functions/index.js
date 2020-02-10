1b6cddd1cab1034c4516780250f56106f2fbeaa4
islom247 <islomiddin.sodiqov@gmail.com>
2020-02-10 19:26:57 +0300

    simple update

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+//starting to move forward
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
