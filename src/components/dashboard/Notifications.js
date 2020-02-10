8663ca13d5e3ea417d409b971199289fecd90263
islom247 <islomiddin.sodiqov@gmail.com>
2020-02-10 22:31:54 +0300

    template updated

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,23 @@
 import React from "react";
-const Notifications = () => {
-  return (
-    <div>
-      <p>Notifications</p>
-    </div>
-  );
+
+const Notifications = (props) => {
+    return (
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                <span className="card-title">
+                    Notifications
+                </span>
+                    <ul className="notifications">
+                        <li>notification1</li>
+                        <li>notification2</li>
+                        <li>notification3</li>
+                        <li>notification4</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 export default Notifications;
