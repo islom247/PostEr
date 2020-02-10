d6dedcf9b318e3325746a1d74985d4fc2d147c5e
islom247 <islomiddin.sodiqov@gmail.com>
2020-02-11 02:05:17 +0300

    ordering projects and notifications by time of creation

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -37,5 +37,9 @@ const mapStateToProps = state => {
 };
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([{collection: "projects"}, {collection: "notifications", limit: 3}])
+    firestoreConnect([{collection: "projects", orderBy: ['createdAt', 'desc']}, {
+        collection: "notifications",
+        limit: 3,
+        orderBy: ['time', 'desc']
+    }])
 )(Dashboard);
